f87856c48d82930eb02b18c29311c4b7617f030b
ZZZank <3410764033@qq.com>
2026-01-29 14:20:36 +0800

    fix 1.21

--- neoforge/src/main/java/zank/mods/open_in_inventory/OpenInInventoryNeoForge.java
+++ neoforge/src/main/java/zank/mods/open_in_inventory/OpenInInventoryNeoForge.java
@@ -5,10 +5,8 @@ package zank.mods.open_in_inventory;
 import net.neoforged.fml.common.Mod;
 
 @Mod(OpenInInventory.ID)
-public class OpenInInventoryNeoForge {
-    public static OpenInInventory COMMON;
-
+public class OpenInInventoryNeoForge extends OpenInInventory {
     public OpenInInventoryNeoForge() {
-        COMMON = new OpenInInventory();
+        COMMON = this;
     }
 }
