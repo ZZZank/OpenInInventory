0e30f621ef9ee55dfd4696ff8c79ce2f06a29417
ZZZank <3410764033@qq.com>
2026-02-03 13:32:40 +0800

    add `open-in-inventory replaceTemplate ...` client command

--- src/main/java/zank/mods/open_in_inventory/impl/handler/ClientEventHandler.java
+++ src/main/java/zank/mods/open_in_inventory/impl/handler/ClientEventHandler.java
@@ -2,17 +2,21 @@ package zank.mods.open_in_inventory.impl.handler;
 
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.serialization.JsonOps;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.command.CommandRegistryAccess;
+import net.minecraft.command.CommandSource;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.ClickEvent;
 import net.minecraft.text.HoverEvent;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import zank.mods.open_in_inventory.OpenInInventory;
 import zank.mods.open_in_inventory.api.ScreenClosedEvent;
+import zank.mods.open_in_inventory.impl.OpenActionRegistryImpl;
 
 import java.util.function.Supplier;
 
@@ -65,6 +69,24 @@ public class ClientEventHandler {
                         });
                     return Command.SINGLE_SUCCESS;
                 }))
+            .then(literal("replaceTemplate")
+                .then(argument("key", StringArgumentType.string())
+                    .suggests((cx, builder) -> CommandSource.suggestMatching(
+                        ((OpenActionRegistryImpl) OpenInInventory.ACTION_REGISTRY).replaceTemplates.keySet(),
+                        builder
+                    ))
+                    .executes(cx -> {
+                        var key = cx.getArgument("key", String.class);
+                        var replace = OpenInInventory.ACTION_REGISTRY.getReplaceTemplate(key);
+                        Supplier<Text> message = () -> Text.empty()
+                            .append(Text.literal(key).setStyle(Style.EMPTY.withColor(Formatting.GREEN)))
+                            .append(" -> ")
+                            .append(replace.toString());
+                        cx.getSource().arch$sendSuccess(message/*?if <1.20 {*//*.get()*//*?}*/, false);
+                        return Command.SINGLE_SUCCESS;
+                    })
+                )
+            )
         );
     }
 }
