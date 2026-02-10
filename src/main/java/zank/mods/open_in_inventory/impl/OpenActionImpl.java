7a60a10ebdd882fceb7bc3e8fd3fdcc13740ba04
ZZZank <3410764033@qq.com>
2026-02-10 13:56:36 +0800

    simplify OpenActionImpl.GsonAdapter

--- src/main/java/zank/mods/open_in_inventory/impl/OpenActionImpl.java
+++ src/main/java/zank/mods/open_in_inventory/impl/OpenActionImpl.java
@@ -4,8 +4,6 @@ import com.google.gson.*;
 import com.google.gson.annotations.JsonAdapter;
 import com.mojang.serialization.JsonOps;
 import net.minecraft.item.ItemStack;
-import net.minecraft.registry.Registries;
-import net.minecraft.util.Identifier;
 import zank.mods.open_in_inventory.OpenInInventory;
 import zank.mods.open_in_inventory.api.OpenAction;
 
@@ -19,19 +17,12 @@ public record OpenActionImpl(ItemStack stack, boolean sneak) implements OpenActi
     private static final boolean SNEAK_DEFAULT = false;
     private static final int COUNT_DEFAULT = 1;
 
-    public static class GsonAdapter implements JsonSerializer<OpenActionImpl>, JsonDeserializer<OpenActionImpl> {
+    public static class GsonAdapter implements JsonDeserializer<OpenActionImpl> {
         private static final String COUNT_KEY = /*? if <1.21 { */"Count"/*? } else {*//*"count"*//*?}*/;
 
         @Override
         public OpenActionImpl deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext context)
             throws JsonParseException {
-            if (jsonElement.isJsonPrimitive()) {
-                var stack = Registries.ITEM
-                    .get(Identifier.tryParse(jsonElement.getAsString()))
-                    .getDefaultStack();
-                return new OpenActionImpl(stack, false);
-            }
-
             var json = jsonElement.getAsJsonObject();
             if (!json.has(COUNT_KEY)) {
                 json.addProperty(COUNT_KEY, COUNT_DEFAULT);
@@ -46,22 +37,6 @@ public record OpenActionImpl(ItemStack stack, boolean sneak) implements OpenActi
             var sneak = sneakJson == null ? SNEAK_DEFAULT : sneakJson.getAsBoolean();
 
             return new OpenActionImpl(stack, sneak);
-        }
-
-        @Override
-        public JsonElement serialize(OpenActionImpl action, Type type, JsonSerializationContext context) {
-            var json = ItemStack.CODEC
-                .encodeStart(JsonOps.INSTANCE, action.stack)
-                .resultOrPartial(OpenInInventory.LOGGER::error)
-                .orElseThrow()
-                .getAsJsonObject();
-            if (action.sneak != SNEAK_DEFAULT) {
-                json.addProperty("sneak", action.sneak);
-            }
-            if (json.has(COUNT_KEY) && json.get(COUNT_KEY).getAsInt() == COUNT_DEFAULT) {
-                json.remove(COUNT_KEY);
-            }
-            return json;
         }
     }
 }
