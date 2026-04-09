53ec4b12384dc17474f0862d842c162d3b1d4300
ZZZank <47418975+ZZZank@users.noreply.github.com>
2026-04-09 22:40:49 +0800

    fix template support in config

--- src/main/java/zank/mods/open_in_inventory/impl/compat/ProvideConfigOpenAction.java
+++ src/main/java/zank/mods/open_in_inventory/impl/compat/ProvideConfigOpenAction.java
@@ -1,5 +1,7 @@
 package zank.mods.open_in_inventory.impl.compat;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonPrimitive;
 import com.mojang.datafixers.util.Pair;
 import com.mojang.serialization.DataResult;
 import com.mojang.serialization.JsonOps;
@@ -19,7 +21,19 @@ public class ProvideConfigOpenAction implements OpenInInventoryPlugin {
 
     @Override
     public void registerAction(OpenActionRegistry registry) {
+        // preprocess
+        var entries = new JsonArray();
         for (var json : OpenInInventory.CONFIG.enabledItems()) {
+            if (json.isJsonPrimitive()) {
+                for (var applied : registry.findAndApplyTemplate(json.getAsString())) {
+                    entries.add(new JsonPrimitive(applied));
+                }
+            } else {
+                entries.add(json);
+            }
+        }
+
+        for (var json : entries) {
             DataResult<? extends OpenAction> result;
             if (json.isJsonPrimitive()) {
                 result = WildCardOpenAction.CODEC.decode(JsonOps.INSTANCE, json)
